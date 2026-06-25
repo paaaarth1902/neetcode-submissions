1bf43fb82e3dd6b43ac01b351714f37fbdb73aaa
Parth <parthprabhune00@gmail.com>
2026-06-25 10:48:39 +0530

    Add: russian-doll-envelopes - submission-0

---
+++ Data Structures & Algorithms/russian-doll-envelopes/submission-0.py
@@ -0,0 +1,42 @@
+class Solution:
+    def search_Insert(self, arr, t):
+        l = 0
+        h = len(arr) - 1
+
+        while l<= h:
+            m = l + (h - l) // 2
+
+            if arr[m] == t:
+                return m
+            elif arr[m] > t:
+                h = m - 1
+            else:
+                l = m + 1
+
+        return l
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+        res = []
+        res.append(nums[0])
+
+        for i in range(1, len(nums)):
+            pos = self.search_Insert(res, nums[i])
+            if pos == len(res):
+                res.append(nums[i])
+            else:
+                res[pos] = nums[i]
+
+        return len(res)
+
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        temp = []
+        for l in envelopes:
+            temp.append(l[1])
+
+        op = self.lengthOfLIS(temp)
+
+        return op
+
+        
